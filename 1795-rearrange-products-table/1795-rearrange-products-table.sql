64b43bb1d20d3adee966787048f8eda8914a8ffb
Andrew Rachuig <102124490+AndrewRachuig@users.noreply.github.com>
2022-09-12 11:18:28 -0500

    Time: 937 ms (19.95%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -2,16 +2,16 @@
 SELECT *
 FROM
     (SELECT product_id, "store1" AS store, store1 as price
-        FROM Products
+    FROM Products
 
-    UNION
+        UNION
 
     SELECT product_id, "store2" AS store, store2 as price
-        FROM Products
+    FROM Products
 
-    UNION
+        UNION
 
     SELECT product_id, "store3" AS store, store3 as price
-        FROM Products) AS compiled
+    FROM Products) AS compiled
     
 WHERE price IS NOT NULL;
