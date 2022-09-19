3a8964a32a966d87fc283f1b268f02d866865f99
Andrew Rachuig <102124490+AndrewRachuig@users.noreply.github.com>
2022-09-19 10:04:41 -0500

    Time: 676 ms (17.29%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -3,3 +3,10 @@ FROM (SELECT email, count(id) as total
         FROM Person
         GROUP BY email) as first
 WHERE total > 1;
+
+
+# Alternate form using HAVING
+# select Email
+# from Person
+# group by Email
+# having count(Email)>1 
