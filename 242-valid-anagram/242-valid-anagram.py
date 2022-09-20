a94758cedcea001c344d73d0f9561c21223b9287
Andrew Rachuig <102124490+AndrewRachuig@users.noreply.github.com>
2022-09-20 09:57:13 -0500

    Time: 102 ms (23.95%), Space: 15.2 MB (11.96%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,14 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        if set(s) != set(t):
-            return False
-        else:
+#      This works but is unneccessarily long. See better solution below
+
+#         if set(s) != set(t):
+#             return False
+#         else:
             
-            s_dict = {letter: 1 for letter in s}
-            s_dict = {letter: s.count(letter) for letter in s}
+#             s_dict = {letter: 1 for letter in s}
+#             s_dict = {letter: s.count(letter) for letter in s}
             
-            t_dict = {letter: 1 for letter in t}
-            t_dict = {letter: t.count(letter) for letter in t}
+#             t_dict = {letter: 1 for letter in t}
+#             t_dict = {letter: t.count(letter) for letter in t}
             
-            return s_dict == t_dict
+#             return s_dict == t_dict
+
+        return sorted(s) == sorted(t)
