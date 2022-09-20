833f712a18de38668ef18a878db9cbac7252c52c
Andrew Rachuig <102124490+AndrewRachuig@users.noreply.github.com>
2022-09-20 10:38:22 -0500

    Time: 52 ms (93.87%), Space: 14.1 MB (53.01%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,6 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        
+         
+#     This works but timed out, so I wrote a more efficient version below.
 #         ransom_dict = {letter: 1 for letter in set(ransomNote)}
 #         ransom_dict = {letter: ransomNote.count(letter) for letter in ransomNote}
         
@@ -17,14 +18,22 @@ class Solution:
 #                 return False
 #         return True
     
-        note_totals = sorted(ransomNote)
-        magazine_totals = sorted(magazine)
+#       This is my second solution that works better but is still slow. Better solution below.
+#         note_totals = sorted(ransomNote)
+#         magazine_totals = sorted(magazine)
         
-        for letter in note_totals:
-            if letter in magazine_totals:
-                magazine_totals.remove(letter)
-                pass
-            else:
+#         for letter in note_totals:
+#             if letter in magazine_totals:
+#                 magazine_totals.remove(letter)
+#                 pass
+#             else:
+#                 return False
+        
+#         return True
+    
+    # I love how elegant this solutions is below
+        for letter in set(ransomNote):
+            if magazine.count(letter) < ransomNote.count(letter):
                 return False
-        
+            
         return True
