afd71dfc550e28ef7c69cbf74df7689227afd8c1
Andrew Rachuig <102124490+AndrewRachuig@users.noreply.github.com>
2022-09-20 09:52:21 -0500

    Time: 9671 ms (5.01%), Space: 14.5 MB (67.16%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if set(s) != set(t):
+            return False
+        else:
+            
+            s_dict = {letter: 1 for letter in s}
+            s_dict = {letter: s.count(letter) for letter in s}
+            
+            t_dict = {letter: 1 for letter in t}
+            t_dict = {letter: t.count(letter) for letter in t}
+            
+            return s_dict == t_dict
